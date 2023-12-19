5f882734b53cc6d1b5cd122f3dc1d2d284c357de
Amir-Muradov <amirmuradov0631@gmail.com>
2023-12-19 09:36:30 +0400

    first javascript

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var name = prompt("Name?");
+
+var surname = prompt("Surname?");
+
+var age =prompt("How old are you?");
+
+var time=25 - age;
+
+alert(" Hello " + name + " " + surname + " " + "in " + time + " " + " you will be 25 years old ");
+
+ console.log(" Hello " + name + " " + surname + " " + "in " + time + " " + " you will be 25 years old ");
